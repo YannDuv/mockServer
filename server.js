298316af79a93854edcc7725173c068a97a79017
Yann DUVAL <yann.duval2@altran.com>
2017-03-01 17:34:38 +0100

    Allowing CORS

--- server.js
+++ server.js
@@ -28,6 +28,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(cookieParser('shhhh, very secret'));
 
+// CORS allowed
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app = require('./app')(app);
 
 app.listen(port, function () {
